f7a418f80e1cc444e6c0bdeef298f873b888985b
Jay Owings <jaydenowings@mail.weber.edu>
2021-04-19 09:13:47 -0600

    created loop for adding cards to deck

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 from Card import Card
 from Minion import Minion
-
 def main():
-
     deck = []
-
-    acornBearer = Minion("Acorn Bearer", 1, 2, 1)
-    deck.append(acornBearer)
-
-    razorBoar = Minion("Razor Boar", 2, 2, 2)
-    deck.append(razorBoar)
-
-    crystalPower = Card("Crystal Power", 1)
-    deck.append(crystalPower)
-
-    vileCall = Card("Vile Call", 3)
-    deck.append(vileCall)
-
+    cards = open("Cards.txt", "r")
+    for card in cards:
+        cardInfo = card.split(',')
+        nameofcard = cardInfo[0]
+        costofcard = cardInfo[1]
+        if len(cardInfo) == 2:
+            cardData = Card(nameofcard, costofcard)
+        if len(cardInfo) == 4:
+            cardattack = cardInfo[2]
+            cardhp = cardInfo[3]
+            cardData = Minion(nameofcard, costofcard, cardattack, cardhp)
+        deck.append (cardData)
+    cards.close()
     for card in deck:
         card.printCard()
         print("\n")
-
-
 if __name__ == "__main__":
     main()
